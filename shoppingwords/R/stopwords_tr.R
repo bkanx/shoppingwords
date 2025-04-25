4670c13ffda2f9e7c88a82497bb87b4189a1c973
bkanx <bkan@eskisehir.edu.tr>
2025-04-24 23:03:10 -0400

    Fixed GitHub repo structure to match local files

--- shoppingwords/R/stopwords_tr.R
+++
@@ -1,8 +0,0 @@
-#' Load stopwords dataset
-#' @param format The file format to load ("csv")
-#' @return A data frame (tibble)
-#' @import readr
-#' @export
-load_stopwords <- function(format = "csv") {
-    return(readr::read_csv(system.file("data", "stopwords_tr.csv", package = "shoppingwords")))
-  }

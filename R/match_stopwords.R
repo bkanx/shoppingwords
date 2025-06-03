e3720d13c9c206213d2a691a35391858f83347e2
bkanx <bkan@eskisehir.edu.tr>
2025-06-03 14:10:13 -0400

    Updated package to latest version from local R project

--- R/match_stopwords.R
+++ R/match_stopwords.R
@@ -1,15 +1,15 @@
-#' Normalize Stopwords in Text
+#' Remove Stopwords from User Reviews
 #'
-#' This function checks whether a given word is present in a predefined stopwords list.
+#' This function processes a dataframe containing user reviews and removes predefined stopwords.
 #' It first searches the package's internal stopwords dataset (`stopwords_tr`), and if
 #' no match is found, it falls back to the broader `stopwords_iso` list.
 #'
-#' The function converts text to a standard format by removing accents and special characters,
-#' transforming it into basic Latin characters, and making all letters lowercase.
-#' This ensures more accurate word comparisons.
+#' The function converts text to a standardized format by removing accents and special characters,
+#' transforming it into basic Latin characters, and making all letters lowercase. It then
+#' tokenizes the text, filters out stopwords, and returns the cleaned version.
 #'
-#' @param text Character string representing the word to normalize.
-#' @return A list containing the matched word and its corresponding index, or `NULL` if no match is found.
+#' @param df Dataframe containing user reviews, with required columns `"comment"` (text) and `"rating"` (numerical score).
+#' @return A modified dataframe with an additional `"cleaned_text"` column containing stopword-free text.
 #' @import stringi
 #' @importFrom stringdist amatch
 #' @import stopwords

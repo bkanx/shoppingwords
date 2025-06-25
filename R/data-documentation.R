54313c33c80b22b5fc9a782a8fd37ab66a5ea493
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2025-06-25 13:37:25 -0400

    Separate out data documentation into individual files

--- R/data-documentation.R
+++ R/data-documentation.R
@@ -6,18 +6,6 @@
 #' @format A tibble with phrases.
 "phrases"
 
-#' A dataset of reviews
-#'
-#' @description User reviews collected from an e-commerce site.
-#' @format A tibble containing text reviews and rating scores.
-"reviews"
-
-#' A dataset of Turkish stopwords
-#'
-#' @description A dataset of stopwords used in Turkish text analysis.
-#' @format A tibble with stopwords.
-"stopwords_tr"
-
 #' A test dataset
 #'
 #' @description A test sample data used for testing analysis functions. It differs from reviews data.
---
+++ R/reviews.R
@@ -0,0 +1,11 @@
+#' A dataset of reviews
+#'
+#' User reviews collected from an e-commerce site.
+#'
+#' @format A tbl_df with with 260,308 rows and 3 variables:
+#' \describe{
+#'   \item{rating}{Rating score, out of 5.}
+#'   \item{comment}{Comment text, in Turkish.}
+#'   \item{id}{Rating ID.}
+#' }
+"reviews"
